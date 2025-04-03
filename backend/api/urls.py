caf8b66534200e6cb4cdd90ee61db4be591e31f1
Rydzze <rydzze@gmail.com>
2025-04-03 22:15:28 +0800

    add API for dashboard data

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,12 +3,18 @@ from rest_framework.routers import DefaultRouter
 from .views import SpiderViewSet, PostViewSet, AnalysisViewSet
 
 router = DefaultRouter()
-router.register(r'spiders', SpiderViewSet, basename='spider')
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'analyses', AnalysisViewSet, basename='analysis')
+router.register(r'spiders', SpiderViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'analyses', AnalysisViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('deploy_crawler/', SpiderViewSet.as_view({'post': 'deploy_crawler'}), name='deploy_crawler'),
-    path('analysis_records/', PostViewSet.as_view({'get': 'analysis_records'}), name='analysis_records')
+    path('analysis_records/', PostViewSet.as_view({'get': 'analysis_records'}), name='analysis_records'),
+    path('summary_stats/', PostViewSet.as_view({'get': 'summary_stats'}), name='summary_stats'),
+    path('top_keywords/', PostViewSet.as_view({'get': 'top_keywords'}), name='top_keywords'),
+    path('top_entities/', PostViewSet.as_view({'get': 'top_entities'}), name='top_entities'),
+    path('posts_over_time/', PostViewSet.as_view({'get': 'posts_over_time'}), name='posts_over_time'),
+    path('posts_by_severity/', AnalysisViewSet.as_view({'get': 'posts_by_severity'}), name='posts_by_severity'),
+    path('posts_by_sentiment/', AnalysisViewSet.as_view({'get': 'posts_by_sentiment'}), name='posts_by_sentiment'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,11 +2,15 @@ from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from main.models import Spider, Post, Analysis
-from api.serializers import SpiderSerializer, PostSerializer, AnalysisSerializer, PostWithAnalysisSerializer
+from api.serializers import *
 from scrapy.crawler import CrawlerRunner
 from scrapy.spiderloader import SpiderLoader
 from scrapy.utils.project import get_project_settings
 from django.db import close_old_connections
+from django.db.models import Count, Case, When, IntegerField
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
+import calendar
 import logging
 import crochet
 
@@ -126,8 +130,123 @@ class PostViewSet(viewsets.ModelViewSet):
         posts = Post.objects.select_related('spider', 'analysis').all()
         serializer = PostWithAnalysisSerializer(posts, many=True)
         return Response(serializer.data)
+    
+    @action(detail=False, methods=['get'])
+    def summary_stats(self, request):
+        total_posts = Post.objects.count()
+        total_threats = Analysis.objects.filter(threat="Yes").count()
+        total_negative_sentiments = Analysis.objects.filter(overall_sentiment='Negative').count()
+        
+        return Response({
+            'total_posts': total_posts,
+            'total_threats': total_threats,
+            'total_negative_sentiments': total_negative_sentiments
+        })
+
+    @action(detail=False, methods=['get'])
+    def top_keywords(self, request):
+        timeframe = timezone.now() - relativedelta(months=1)
+        keyword_stats = Post.objects.filter(timestamp__gte=timeframe) \
+                                .values('spider__keyword') \
+                                .annotate(total=Count('id')) \
+                                .order_by('-total')[:5]
+        
+        return Response([
+            {'label': item['spider__keyword'], 'value': item['total']} 
+            for item in keyword_stats
+        ])
+
+    @action(detail=False, methods=['get'])
+    def top_entities(self, request):
+        timeframe = timezone.now() - relativedelta(months=1)
+        entity_stats = Post.objects.filter(timestamp__gte=timeframe) \
+                            .values('username') \
+                            .annotate(total=Count('id')) \
+                            .order_by('-total')[:5]
+        
+        return Response([
+            {'label': item['username'], 'value': item['total']} 
+            for item in entity_stats
+        ])
+    
+    @action(detail=False, methods=['get'])
+    def posts_over_time(self, request):
+        six_months_ago = timezone.now() - relativedelta(months=6)
+        clear_web = ['Reddit']
+        dark_web = ['DarkNet Army', 'Best Carding World']
+        
+        recent_posts = Post.objects.filter(timestamp__gte=six_months_ago).select_related('spider')
+        current_date = timezone.now()
+        result = []
+        months = []
+
+        for i in range(5, -1, -1):
+            month_date = current_date - relativedelta(months=i)
+            months.append((month_date.month, month_date.year, calendar.month_name[month_date.month]))
+        
+        for month_num, year, month_name in months:
+            month_posts = recent_posts.filter(
+                timestamp__month=month_num,
+                timestamp__year=year
+            )
+            
+            clear_web_posts = month_posts.filter(spider__spider_name__in=clear_web).count()
+            dark_web_posts = month_posts.filter(spider__spider_name__in=dark_web).count()
+            result.append({
+                "month": month_name,
+                "clear_web": clear_web_posts,
+                "dark_web": dark_web_posts
+            })
+        
+        return Response(result)
 
 class AnalysisViewSet(viewsets.ModelViewSet):
     queryset = Analysis.objects.all()
     serializer_class = AnalysisSerializer
     permission_classes = [permissions.AllowAny]
+
+    @action(detail=False, methods=['get'])
+    def posts_by_severity(self, request):
+        timeframe = timezone.now() - relativedelta(months=1)
+        recent_post = Post.objects.filter(timestamp__gte=timeframe).values_list('id', flat=True)
+
+        category_stats = Analysis.objects.filter(post_id__in=recent_post).aggregate(
+            critical=Count(Case(When(confidence__gt=85, then=1), output_field=IntegerField())),
+            high=Count(Case(When(confidence__gt=70, confidence__lte=85, then=1), output_field=IntegerField())),
+            medium=Count(Case(When(confidence__gt=60, confidence__lte=70, then=1), output_field=IntegerField())),
+            low=Count(Case(When(confidence__gt=50, confidence__lte=60, then=1), output_field=IntegerField())),
+            neutral=Count(Case(When(confidence__lte=50, then=1), output_field=IntegerField()))
+        )
+
+        result = []        
+        severity_label = ['critical', 'high', 'medium', 'low', 'neutral']
+        for item in severity_label:
+            result.append({
+                "label": item,
+                "posts": category_stats[item],
+                "fill": f"var(--color-{item})"
+            })
+
+        return Response(result)
+    
+    @action(detail=False, methods=['get'])
+    def posts_by_sentiment(self, request):
+        timeframe = timezone.now() - relativedelta(months=1)
+        recent_post = Post.objects.filter(timestamp__gte=timeframe).values_list('id', flat=True)
+        
+        sentiment_stats = Analysis.objects.filter(post_id__in=recent_post) \
+                                         .values('overall_sentiment') \
+                                         .annotate(total=Count('id'))
+        
+        result = []
+        for item in sentiment_stats:
+            sentiment = item['overall_sentiment']
+            count = item['total']
+            
+            result.append({
+                "label": sentiment.lower(),
+                "posts": count,
+                "fill": f"var(--color-{sentiment.lower()})"
+            })
+        
+        return Response(result)
